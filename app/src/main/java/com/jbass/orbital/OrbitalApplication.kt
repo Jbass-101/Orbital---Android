93d358d43f9417c2294579ed025d7f06024d46ce
Junior Baloji <junior07bj@gmail.com>
2026-01-26 23:47:45 +0200

    Dagger-Hilt Application

---
+++ app/src/main/java/com/jbass/orbital/OrbitalApplication.kt
@@ -0,0 +1,12 @@
+package com.jbass.orbital
+
+
+import android.app.Activity
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class OrbitalApplication: Application(){
+
+}
