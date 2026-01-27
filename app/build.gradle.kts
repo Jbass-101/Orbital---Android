8b8e15a5a46684eea2f4ac19e7d3a837f6d5d449
Junior <junior07bj@gmail.com>
2026-01-27 15:34:38 +0200

    Added Icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    //Icons
+    implementation(libs.androidx.compose.icons)
 
      //Ktor
     implementation(platform(libs.ktor.bom))
