1b845c1f8b2f4b7c4fa74295e1f6e2a68acfc7b4
Junior Baloji <junior07bj@gmail.com>
2026-02-02 00:00:23 +0200

    Cleaned up

--- app/src/main/java/com/jbass/orbital/MainActivity.kt
+++ app/src/main/java/com/jbass/orbital/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import com.jbass.orbital.presentation.dashboard.DashboardScreen
 import com.jbass.orbital.presentation.navigation.OrbitalNavHost
 import com.jbass.orbital.ui.theme.OrbitalTheme
 import dagger.hilt.android.AndroidEntryPoint
