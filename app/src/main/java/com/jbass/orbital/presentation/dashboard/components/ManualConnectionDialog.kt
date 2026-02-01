d394a2e73e84ca7dcd0821a18559dc9eb861ff2a
Junior Baloji <junior07bj@gmail.com>
2026-02-01 23:58:17 +0200

    Added Preview

--- app/src/main/java/com/jbass/orbital/presentation/dashboard/components/ManualConnectionDialog.kt
+++ app/src/main/java/com/jbass/orbital/presentation/dashboard/components/ManualConnectionDialog.kt
@@ -13,7 +13,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jbass.orbital.ui.theme.OrbitalTheme
 
 @Composable
 fun ManualConnectionDialog(
@@ -42,4 +44,12 @@ fun ManualConnectionDialog(
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun previewManualConnection(){
+    OrbitalTheme() {
+        ManualConnectionDialog {  }
+    }
 }
