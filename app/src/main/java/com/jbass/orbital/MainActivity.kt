03df26d6e67da065349b40e65d5ee63a512c4273
Junior Baloji <junior07bj@gmail.com>
2026-02-03 00:01:30 +0200

    Updated navhost

--- app/src/main/java/com/jbass/orbital/MainActivity.kt
+++ app/src/main/java/com/jbass/orbital/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.jbass.orbital.presentation.navigation.OrbitalNavHost
+import com.jbass.orbital.presentation.navigation.OrbitalNavigation
 import com.jbass.orbital.ui.theme.OrbitalTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OrbitalTheme {
-                OrbitalNavHost()
+                OrbitalNavigation()
             }
         }
     }
--- app/src/main/java/com/jbass/orbital/presentation/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/jbass/orbital/presentation/navigation/NavigationRoutes.kt
@@ -1,29 +1,8 @@
 package com.jbass.orbital.presentation.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.ui.graphics.vector.ImageVector
-
-sealed class Screen(
-    val route: String,
-    val label: String,
-    val icon: ImageVector
-) {
-    object Dashboard : Screen(
-        route = "home",
-        label = "Home",
-        icon = Icons.Default.Home
-    )
-
-
-    object Rooms : Screen("rooms", "Rooms", Icons.Default.Home)
-
-    data object CategoryDevices : Screen(
-        route = "c/{roomId}/{category}",
-        label = "Home",
-        icon = Icons.Default.Home
-    ) {
-        fun createRoute(roomId: String, category: String): String =
-            "category_devices/$roomId/$category"
+sealed class Screen(val route: String) {
+    object Dashboard : Screen("dashboard")
+    object RoomDetail : Screen("room_detail/{roomId}") {
+        fun createRoute(roomId: String) = "room_detail/$roomId"
     }
 }
